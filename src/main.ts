d384ecd004af1ce802eb89b7557d54adaf66ebbb
小白萌新 <3036403996@qq.com>
2024-03-17 16:29:53 +0800

    feat: install vant

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import App from './App.vue'
 import router from './router'
 import '@/styles/main.scss'
 
+// vant组件
+// 1. 引入你需要的组件
+import { Button } from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Button)
 
 app.mount('#app')
