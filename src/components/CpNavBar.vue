df8a12120636e33c08bc241aba9ddeb85ddf5310
小白萌新 <3036403996@qq.com>
2024-03-18 17:07:44 +0800

    feat: 二次封装 NavBar 组件，支持自定义标题和事件

---
+++ src/components/CpNavBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  title?: string
+  rightText?: string
+}
+
+// 接收父组件的值
+defineProps<Props>()
+
+interface Emits {
+  (e: 'click-right'): void
+}
+const emit = defineEmits<Emits>()
+</script>
+
+<template>
+  <van-nav-bar
+    left-arrow
+    fixed
+    placeholder
+    :title="title"
+    :right-text="rightText"
+    @click-right="emit('click-right')"
+  />
+</template>
+
+<style lang="scss" scoped>
+::v-deep() {
+  .van-nav-bar {
+    &__arrow {
+      font-size: 18px;
+      color: var(--cp-text1);
+    }
+    &__text {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import CpNavBar from '@/components/CpNavBar.vue'
+const fn = () => {
+  console.log('fn调用成功')
+}
+</script>
 
 <template>
-  <div class="login-page">登录组件</div>
+  <div class="login-page">
+    <CpNavBar title="登录" right-text="注册" @click-right="fn" />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
