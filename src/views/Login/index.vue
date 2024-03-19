682678e9b8e69bcbb7827761c20be6e2d1ac19c6
小白萌新 <3036403996@qq.com>
2024-03-19 16:54:36 +0800

    feat: 登录模块静态结构和表单校验规则完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,23 +1,116 @@
 <script setup lang="ts">
+import { mobileRules, passwordRules } from '@/utils/rules'
+import { showToast } from 'vant'
 import { ref } from 'vue'
+// 表单校验
+const mobile = ref('')
+const password = ref('')
 
-const gender = ref(0)
+// 表单提交，通过校验规则后才会触发
+const onSubmit = () => {
+  showToast('提交成功')
+}
 </script>
 
 <template>
   <div class="login-page">
-    <CpNavBar title="登录" right-text="注册" />
-    <CpRadioBtn
-      v-model="gender"
-      :options="[
-        { label: '男', value: 0 },
-        { label: '女', value: 1 },
-        { label: '未知', value: 2 }
-      ]"
-    />
-    <CpIcon name="login-eye-off" />
-    <CpIcon name="login-eye-on" />
+    <!-- 导航栏 -->
+    <cp-nav-bar right-text="注册"></cp-nav-bar>
+
+    <div class="login-head">
+      <h3>密码登录</h3>
+      <a href="javascript:;">
+        <span>短信验证码登录</span>
+        <van-icon name="arrow"></van-icon>
+      </a>
+    </div>
+
+    <!-- form 表单 -->
+    <van-form autocomplete="off" @submit="onSubmit">
+      <van-field
+        v-model="mobile"
+        :rules="mobileRules"
+        placeholder="请输入手机号"
+        type="tel"
+      ></van-field>
+      <van-field
+        v-model="password"
+        :rules="passwordRules"
+        placeholder="请输入密码"
+        type="password"
+      ></van-field>
+      <div class="cp-cell">
+        <van-checkbox>
+          <span>我已同意</span>
+          <a href="javascript:;">用户协议</a>
+          <span>及</span>
+          <a href="javascript:;">隐私条款</a>
+        </van-checkbox>
+      </div>
+      <div class="cp-cell">
+        <van-button block round type="primary" native-type="submit">登 录</van-button>
+      </div>
+      <div class="cp-cell">
+        <a href="javascript:;">忘记密码？</a>
+      </div>
+    </van-form>
+
+    <!-- 第三方登录 -->
+    <div class="login-other">
+      <van-divider>第三方登录</van-divider>
+      <div class="icon">
+        <img src="@/assets/qq.svg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  &-head {
+    padding: 30px 30px 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    line-height: 1;
+    h3 {
+      font-weight: normal;
+      font-size: 24px;
+    }
+    a {
+      font-size: 15px;
+    }
+  }
+  &-other {
+    margin-top: 60px;
+    padding: 0 30px;
+    .icon {
+      display: flex;
+      justify-content: center;
+      img {
+        width: 36px;
+        height: 36px;
+        padding: 4px;
+      }
+    }
+  }
+}
+
+.van-form {
+  padding: 0 14px;
+  .cp-cell {
+    height: 52px;
+    line-height: 24px;
+    padding: 14px 16px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .van-checkbox {
+      a {
+        color: var(--cp-primary);
+        padding: 0 5px;
+      }
+    }
+  }
+}
+</style>
